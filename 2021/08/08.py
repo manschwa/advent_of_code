0872853f0ef8a54dcf26af19fb1c94bad4d08785
Manuel Schwarz <manschwa@uos.de>
2021-12-07 14:04:51 +0100

    Adding solution for 2021-07

--- 2021/08/08.py
+++ 2021/08/08.py
@@ -7,9 +7,9 @@ print("             ,    *********    ,             ")
 print("             |  *************  |             ")
 print("         ,   Advent of Code 2021   ,         ")
 print("         | *********************** |         ")
-print("     ,   ***************************   ,     ")
+print("     ,   *** Day 07:  ***   ,     ")
 print("     | ******************************* |     ")
-print("     * Day 07: The Treachery of Whales *     ")
+print("     ***********************************     ")
 print("                     ░░░                     ")
 print("                     ░░░                     ")
 print("                                             ")
@@ -17,25 +17,14 @@ print("                                             ")
 filename = "07.input"
 with open(filename) as file:
     raw = file.read().splitlines()
-positions = list(map(int, raw[0].split(',')))
+line = list(map(int, raw[0].split(',')))
 
+def part_one(data):
+    return 23
 
-def calc_fuel(positions, simple = True):
-    min_fuel = float('inf')
-    fuel = [0] * len(positions)
+def part_two(data):
+    return 42
 
-    for i in range(max(positions)):
-        for j in range(len(fuel)):
-            if simple:
-                fuel[j] = abs(positions[j] - i)
-            else:
-                fuel[j] = sum(range(abs(positions[j] - i) + 1))
-        if sum(fuel) < min_fuel:
-            min_fuel = sum(fuel)
-
-    return min_fuel
-
-
-print("Part 1: The amount of fuel needed is {}".format(calc_fuel(positions)))
-print("Part 2: The amount of fuel needed is {}".format(calc_fuel(positions, False)))
+print("Part 1: ", part_one())
+print("Part 2: ", part_two())
 

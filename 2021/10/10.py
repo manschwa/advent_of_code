a76619de57e876ea2501f593418759eb5b063be4
Manuel Schwarz <manschwa@uos.de>
2021-12-09 11:48:04 +0100

    Adding solution for 2021-09 part 1

--- 2021/10/10.py
+++ 2021/10/10.py
@@ -7,37 +7,24 @@ print("             ,    *********    ,             ")
 print("             |  *************  |             ")
 print("         ,   Advent of Code 2021   ,         ")
 print("         | *********************** |         ")
-print("     ,   *** Day 9: Smoke Basin  ***   ,     ")
+print("     ,   *** Day 07:  ***   ,     ")
 print("     | ******************************* |     ")
 print("     ***********************************     ")
 print("                     ░░░                     ")
 print("                     ░░░                     ")
 print("                                             ")
 
-filename = "09.sample"
+filename = "07.input"
 with open(filename) as file:
     raw = file.read().splitlines()
-matrix = raw.copy()
+line = list(map(int, raw[0].split(',')))
 
-def check_sink(matrix, row, col):
-    up    = int(matrix[row - 1][col]) if row > 0 else float('inf')
-    down  = int(matrix[row][col - 1]) if col > 0 else float('inf')
-    left  = int(matrix[row][col + 1]) if col < (len(matrix[0]) - 1) else float('inf')
-    right = int(matrix[row + 1][col]) if row < (len(matrix) -1) else float('inf')
-    neighbors = [up, down, left, right]
-    return int(matrix[row][col]) < min(neighbors)
-
-def part_one(matrix):
-    sinks = []
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if check_sink(matrix, i, j):
-                sinks.append(int(matrix[i][j]))
-    return sum(sinks) + len(sinks)
+def part_one(data):
+    return 23
 
 def part_two(data):
     return 42
 
-print("Part 1: ", part_one(matrix))
-print("Part 2: ", part_two(matrix))
+print("Part 1: ", part_one())
+print("Part 2: ", part_two())
 

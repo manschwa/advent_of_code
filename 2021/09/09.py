7510f1f91f45f2ebd05f7b0f3a65a024a5b1e9da
Manuel Schwarz <manschwa@uos.de>
2021-12-09 23:51:27 +0100

    Adding solution for 2021-9 part 2.

--- 2021/09/09.py
+++ 2021/09/09.py
@@ -1,3 +1,5 @@
+import math
+
 print("                                             ")
 print("                      △                      ")
 print("                    ◁ ☆ ▷                    ")
@@ -14,18 +16,34 @@ print("                     ░░░                     ")
 print("                     ░░░                     ")
 print("                                             ")
 
-filename = "09.sample"
+filename = "09.input"
 with open(filename) as file:
     raw = file.read().splitlines()
 matrix = raw.copy()
 
 def check_sink(matrix, row, col):
     up    = int(matrix[row - 1][col]) if row > 0 else float('inf')
-    down  = int(matrix[row][col - 1]) if col > 0 else float('inf')
-    left  = int(matrix[row][col + 1]) if col < (len(matrix[0]) - 1) else float('inf')
-    right = int(matrix[row + 1][col]) if row < (len(matrix) -1) else float('inf')
+    left  = int(matrix[row][col - 1]) if col > 0 else float('inf')
+    right  = int(matrix[row][col + 1]) if col < (len(matrix[0]) - 1) else float('inf')
+    down = int(matrix[row + 1][col]) if row < (len(matrix) -1) else float('inf')
     neighbors = [up, down, left, right]
     return int(matrix[row][col]) < min(neighbors)
+
+def basin_size(matrix, row, col):
+    sum = 0
+    if int(matrix[row][col]) == 9:
+        return 0
+    else:
+        matrix[row] = matrix[row][:col] + '9' + matrix[row][col + 1:]
+        if row > 0:
+            sum += basin_size(matrix, row - 1, col)
+        if col < (len(matrix[0]) - 1):
+            sum += basin_size(matrix, row, col + 1)
+        if row < (len(matrix) - 1):
+            sum += basin_size(matrix, row + 1, col)
+        if col > 0:
+            sum += basin_size(matrix, row, col - 1)
+    return sum + 1
 
 def part_one(matrix):
     sinks = []
@@ -35,9 +53,15 @@ def part_one(matrix):
                 sinks.append(int(matrix[i][j]))
     return sum(sinks) + len(sinks)
 
-def part_two(data):
-    return 42
+def part_two(matrix):
+    sinks = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if check_sink(matrix, i, j):
+                sinks.append(basin_size(matrix.copy(), i, j))
+    sinks.sort()
+    return math.prod(sinks[-3:])
 
 print("Part 1: ", part_one(matrix))
-print("Part 2: ", part_two(matrix))
+print("Part 2: ", part_two(matrix.copy()))
 

03a09f0a2f0697877f6a4209c6195f1054281f60
Manuel Schwarz <manschwa@uos.de>
2021-12-10 15:49:42 +0100

    Adding solution for 2021-10.

--- 2021/10/10.py
+++ 2021/10/10.py
@@ -7,24 +7,81 @@ print("             ,    *********    ,             ")
 print("             |  *************  |             ")
 print("         ,   Advent of Code 2021   ,         ")
 print("         | *********************** |         ")
-print("     ,   *** Day 07:  ***   ,     ")
+print("     ,   ** Day 10: Syntax Scoring *   ,     ")
 print("     | ******************************* |     ")
 print("     ***********************************     ")
 print("                     ░░░                     ")
 print("                     ░░░                     ")
 print("                                             ")
 
-filename = "07.input"
+filename = "10.input"
 with open(filename) as file:
     raw = file.read().splitlines()
-line = list(map(int, raw[0].split(',')))
+lines = raw.copy()
+
+OPENINGS = ['(', '[', '{', '<']
+CLOSINGS = [')', ']', '}', '>']
+
+def incomplete(line):
+    stack = []
+    for char in line:
+        if char in OPENINGS:
+            stack.append(char)
+        else:
+            if stack[-1] == OPENINGS[CLOSINGS.index(char)]:
+                stack.pop()
+            else:
+                return False
+    return True
+
+
+def incomplete_score(line):
+    stack = []
+    incomplete_score = 0
+    scores = {')' : 1,
+              ']' : 2,
+              '}' : 3,
+              '>' : 4}
+    for char in line:
+        if char in OPENINGS:
+            stack.append(char)
+        else:
+            stack.pop()
+
+    for i in range(len(stack)):
+        incomplete_score = (incomplete_score * 5) + scores[CLOSINGS[OPENINGS.index(stack.pop())]]
+    return incomplete_score
+
+def corrupt_score(line):
+    stack = []
+    scores = {')' : 3,
+              ']' : 57,
+              '}' : 1197,
+              '>' : 25137}
+    for char in line:
+        if char in OPENINGS:
+            stack.append(char)
+        else:
+            if stack[-1] == OPENINGS[CLOSINGS.index(char)]:
+                stack.pop()
+            else:
+                return scores[char]
+    return 0
 
 def part_one(data):
-    return 23
+    score = 0
+    for line in data:
+        score += corrupt_score(line)
+    return score
 
 def part_two(data):
-    return 42
+    scores = []
+    for line in data:
+        if incomplete(line):
+            scores.append(incomplete_score(line))
+    scores.sort()
+    return scores[int(len(scores) / 2)]
 
-print("Part 1: ", part_one())
-print("Part 2: ", part_two())
+print("Part 1: ", part_one(lines))
+print("Part 2: ", part_two(lines))
 

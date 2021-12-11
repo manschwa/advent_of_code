72a16322c37f3752b0f8045c1aedb2401d37ae99
Manuel Schwarz <manschwa@uos.de>
2021-12-11 02:56:57 +0100

    Adding and updating solution for 2020-04.

--- 2020/04/04.py
+++ 2020/04/04.py
@@ -1,3 +1,5 @@
+import re
+
 print("                                             ")
 print("                      *                      ")
 print("                     ***                     ")
@@ -15,27 +17,58 @@ print("                     ***                     ")
 print("                                             ")
 
 filename = "04.input"
-REQ_FIELDS = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid'}
-valid_passports = 0
-passport_fields = {'cid'}
-passport_cnt = 0
-
 with open(filename) as file:
-    for line in file:
-        if(line.strip()):
-            pairs = line.split()
-            for pair in pairs:
-                passport_fields.add(pair.split(':')[0])
-        else:
-            passport_cnt += 1
-            if(REQ_FIELDS == passport_fields):
-                valid_passports += 1
-            passport_fields = {'cid'}
-# extra check for the last passport
-passport_cnt += 1
-if(REQ_FIELDS == passport_fields):
-    valid_passports += 1
+    raw = file.read().splitlines()
 
-print("Number of passports:", passport_cnt)
-print("Number of valid passports:", valid_passports)
+REQ_FIELDS = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid'}
+PATTERNS = {'byr' : '^19[2-9][\d]$|^200[0-2]$',
+            'iyr' : '^201[\d]$|^2020$',
+            'eyr' : '^202[\d]$|^2030$',
+            'hgt' : '^1([5-8][\d]|9[0-3])cm$|^(59|6[\d]|7[0-6])in$',
+            'hcl' : '^#[\da-f]{6}$',
+            'ecl' : '^amb$|^blu$|^brn$|^gr(y|n)$|^hzl$|^oth$',
+            'pid' : '^\d{9}$'}
 
+passports = []
+passport = []
+for line in raw:
+    if not line:
+        passports.append(passport)
+        passport = []
+    else:
+        passport.extend(line.split())
+passports.append(passport)
+
+
+def has_fields(passport):
+    passport_fields = {'cid'}
+    for field in passport:
+        passport_fields.add(field.split(':')[0])
+    return passport_fields == REQ_FIELDS
+
+def is_valid(passport):
+    if has_fields(passport):
+        for attribute in passport:
+            match attribute.split(':')[0]:
+                case 'byr': pattern = PATTERNS['byr']
+                case 'iyr': pattern = PATTERNS['iyr']
+                case 'eyr': pattern = PATTERNS['eyr']
+                case 'hgt': pattern = PATTERNS['hgt']
+                case 'hcl': pattern = PATTERNS['hcl']
+                case 'ecl': pattern = PATTERNS['ecl']
+                case 'pid': pattern = PATTERNS['pid']
+                case _ : pattern = ''
+            attr = attribute.split(':')[1]
+            if not re.match(pattern, attr):
+                return False
+        return True
+    return False
+
+def part_one(passports):
+    return sum(has_fields(passport) for passport in passports)
+
+def part_two(passports):
+    return sum(is_valid(passport) for passport in passports)
+
+print("Part 1: Number of valid passports:", part_one(passports))
+print("Part 2: Number of valid passports:", part_two(passports))

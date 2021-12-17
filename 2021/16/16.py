02006798d1469d9c5040899836b63a6cb7881a71
Manuel Schwarz <manschwa@uos.de>
2021-12-17 02:37:33 +0100

    Adding skeleton files for 2021-16.

---
+++ 2021/16/16.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+print("                                             ")
+print("                      △                      ")
+print("                    ◁ ☆ ▷                    ")
+print("                  ,   *   ,                  ")
+print("                  | ***** |                  ")
+print("             ,    *********    ,             ")
+print("             |  *************  |             ")
+print("         ,   Advent of Code 2021   ,         ")
+print("         | *********************** |         ")
+print("     ,   ** Day 16: Packet Decoder *   ,     ")
+print("     | ******************************* |     ")
+print("     ***********************************     ")
+print("                     ░░░                     ")
+print("                     ░░░                     ")
+print("                                             ")
+
+filename = "16.sample"
+with open(filename) as file:
+    raw = file.read().splitlines()
+
+code = raw[0]
+
+
+def part_one():
+    return 'part 1'
+
+def part_two():
+    return 'part 2'
+
+print("Part 1:", part_one())
+print("Part 2:", part_two())

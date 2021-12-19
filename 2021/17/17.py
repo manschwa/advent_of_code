e9e1a23b598880b6775e44729eb488ec4ec10e3a
Manuel Schwarz <manschwa@uos.de>
2021-12-20 00:09:28 +0100

    Adding solution for 2021-17 part 1.

--- 2021/17/17.py
+++ 2021/17/17.py
@@ -1,6 +1,3 @@
-from collections import Counter
-import numpy as np
-
 print("                                             ")
 print("                      △                      ")
 print("                    ◁ ☆ ▷                    ")
@@ -17,13 +14,37 @@ print("                     ░░░                     ")
 print("                     ░░░                     ")
 print("                                             ")
 
+def check_hit(x, y, x_range, y_range):
+    return (x in x_range and y in y_range)
+
+def throw(x, y, x_range, y_range):
+    y_max = 0
+    x_step = x
+    y_step = y
+    while x <= max(x_range) and y >= min(y_range):
+        if y > y_max:
+            y_max = y
+        if check_hit(x, y, x_range, y_range):
+            return y_max
+        if x_step > 0:
+            x_step -= 1
+        y_step -= 1
+        x += x_step
+        y += y_step
+    return False
 
 
-def part_one():
-    return 'part 1'
+def part_one(x_range, y_range):
+    max_height = 0
+    for x in range(0, max(x_range)):
+        for y in range(min(y_range), max(x_range)):
+            if height := throw(x, y, x_range, y_range):
+                if height > max_height:
+                    max_height = height
+    return max_height
 
 def part_two():
     return 'part 2'
 
-print("Part 1:", part_one())
+print("Part 1:", part_one(range(135, 156), range(-102, -77)))
 print("Part 2:", part_two())

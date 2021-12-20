998795b2ee29b1c9928c1a83e36003336a4e7fcd
Manuel Schwarz <manschwa@uos.de>
2021-12-20 09:47:42 +0100

    Adding skeleton files for 2021-20.

---
+++ 2021/20/20.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+print("                                             ")
+print("                      △                      ")
+print("                    ◁ ☆ ▷                    ")
+print("                  ,   *   ,                  ")
+print("                  | ***** |                  ")
+print("             ,    *********    ,             ")
+print("             |  *************  |             ")
+print("         ,   Advent of Code 2021   ,         ")
+print("         | *********************** |         ")
+print("     ,   *** Day 20: Trench Map  ***   ,     ")
+print("     | ******************************* |     ")
+print("     ***********************************     ")
+print("                     ░░░                     ")
+print("                     ░░░                     ")
+print("                                             ")
+
+
+
+def part_one():
+    return 'part 1'
+
+def part_two():
+    return 'part 2'
+
+print("Part 1:", part_one())
+print("Part 2:", part_two())

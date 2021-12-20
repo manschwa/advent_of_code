afcebd3119ae62a4c7182db85dc418e4b931f62f
Manuel Schwarz <manschwa@uos.de>
2021-12-20 01:33:16 +0100

    Adding solution for 2021-17 part 2 + refactoring.

--- 2021/17/17.py
+++ 2021/17/17.py
@@ -14,10 +14,15 @@ print("                     ░░░                     ")
 print("                     ░░░                     ")
 print("                                             ")
 
+HEIGHTS = []
+HITS = []
+
 def check_hit(x, y, x_range, y_range):
     return (x in x_range and y in y_range)
 
 def throw(x, y, x_range, y_range):
+    initial_x = x
+    initial_y = y
     y_max = 0
     x_step = x
     y_step = y
@@ -25,26 +30,19 @@ def throw(x, y, x_range, y_range):
         if y > y_max:
             y_max = y
         if check_hit(x, y, x_range, y_range):
-            return y_max
+            if (initial_x, initial_y) not in HITS:
+                HITS.append((initial_x, initial_y))
+            HEIGHTS.append(y_max)
         if x_step > 0:
             x_step -= 1
         y_step -= 1
         x += x_step
         y += y_step
-    return False
-
 
 def part_one(x_range, y_range):
-    max_height = 0
-    for x in range(0, max(x_range)):
-        for y in range(min(y_range), max(x_range)):
-            if height := throw(x, y, x_range, y_range):
-                if height > max_height:
-                    max_height = height
-    return max_height
+    for x in range(0, max(x_range) + 1):
+        for y in range(min(y_range), max(x_range) + 1):
+            throw(x, y, x_range, y_range)
+    return max(HEIGHTS), len(HITS)
 
-def part_two():
-    return 'part 2'
-
-print("Part 1:", part_one(range(135, 156), range(-102, -77)))
-print("Part 2:", part_two())
+print("Part 1, 2:", part_one(range(135, 156), range(-102, -77)))

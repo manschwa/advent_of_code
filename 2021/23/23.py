ae749259c69fcab39b87e10c5921a5c912de8729
Manuel Schwarz <manschwa@uos.de>
2021-12-23 21:39:08 +0100

    Adding skeleton files for 2021-23.

---
+++ 2021/23/23.py
@@ -0,0 +1,29 @@
+print("                                             ")
+print("                      △                      ")
+print("                    ◁ ☆ ▷                    ")
+print("                  ,   *   ,                  ")
+print("                  | ***** |                  ")
+print("             ,    *********    ,             ")
+print("             |  *************  |             ")
+print("         ,   Advent of Code 2021   ,         ")
+print("         | *********************** |         ")
+print("     ,   ***** Day 23: Amphipod ****   ,     ")
+print("     | ******************************* |     ")
+print("     ***********************************     ")
+print("                     ░░░                     ")
+print("                     ░░░                     ")
+print("                                             ")
+
+filename = "23.sample"
+with open(filename) as file:
+    raw = file.read().splitlines()
+
+
+def part_one():
+    return 'part 1'
+
+def part_two():
+    return 'part 2'
+
+print("Part 1:", part_one())
+print("Part 2:", part_two())

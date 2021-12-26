006f7cc161a8237523e2da55f85b9f9e62144e34
Manuel Schwarz <manschwa@uos.de>
2021-12-26 20:19:53 +0100

    Adding skeleton files for 2021-25.

---
+++ 2021/25/25.py
@@ -0,0 +1,29 @@
+print("                                             ")
+print("                      △                      ")
+print("                    ◁ ☆ ▷                    ")
+print("                  ,   *   ,                  ")
+print("                  | ***** |                  ")
+print("             ,    *********    ,             ")
+print("             |  *************  |             ")
+print("         ,   Advent of Code 2021   ,         ")
+print("         | *********************** |         ")
+print("     ,   *** Day 25: Sea Cucumber **   ,     ")
+print("     | ******************************* |     ")
+print("     ***********************************     ")
+print("                     ░░░                     ")
+print("                     ░░░                     ")
+print("                                             ")
+
+filename = "25.sample"
+with open(filename) as file:
+    raw = file.read().splitlines()
+
+
+def part_one():
+    return 'part 1'
+
+def part_two():
+    return 'part 2'
+
+print("Part 1:", part_one())
+print("Part 2:", part_two())

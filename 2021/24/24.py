5c0cad95a53d63ea5f98599bd59d4dafbe3cdb83
Manuel Schwarz <manschwa@uos.de>
2021-12-28 02:01:13 +0100

    Adding brute force solution for 2021-24 part 1, not practical.

--- 2021/24/24.py
+++ 2021/24/24.py
@@ -14,10 +14,52 @@ print("                     ░░░                     ")
 print("                     ░░░                     ")
 print("                                             ")
 
-filename = "24.sample"
+filename = "24.input"
 with open(filename) as file:
     raw = file.read().splitlines()
+instructions = []
+for instruction in raw:
+    instructions.append(instruction.split())
 
+
+def add(a, b):
+    return a + b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return int(a / b)
+
+def mod(a ,b):
+    return a % b
+
+def eql(a, b):
+    return a == b
+
+for code in range(99999999999999, 1, -1):
+    if code % 10 == 0:
+        continue
+
+    vars = set()
+    for i in instructions:
+        vars.add(i[1])
+        exec(i[1] + '= 0')
+
+    code = str(code)
+    for i in instructions:
+        if i[0] == 'inp':
+            exec(i[1] + '=' + code[0])
+            if len(code) > 1:
+                code = code[1:]
+        else:
+            exec(i[1] + '=' + str(eval(i[0] + '(' + i[1] + ',' + i[2] + ')')))
+    if z == 0:
+        print("z == 0, code =", code)
+        break
+
+for var in vars:
+    print(var, eval(var))
 
 def part_one():
     return 'part 1'

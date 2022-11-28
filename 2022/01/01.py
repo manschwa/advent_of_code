ff162663006eed671190c1d0a76198b7402e0491
Manuel Schwarz <manschwa@uos.de>
2022-11-28 23:35:05 +0100

    Initial commit for the Advent of Code 2022 \o/

---
+++ 2022/01/01.py
@@ -0,0 +1,20 @@
+print("                                             ")
+print("         ***************************         ")
+print("         **  Advent of Code 2022  **         ")
+print("         ***************************         ")
+print("         **  Day 01: Sonar Sweep  **         ")
+print("         ***************************         ")
+print("                                             ")
+
+filename = "01.input"
+with open(filename) as file:
+    lines = file.read().splitlines()
+
+def part_one():
+    return true
+
+def part_two():
+    return true
+
+print("Part 1: ", part_one())
+print("Part 2: ", part_two())

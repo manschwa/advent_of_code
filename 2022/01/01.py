02506d5d9c1ec58885a39f4758f0b37f8911877c
Manuel Schwarz <manschwa@uos.de>
2022-11-29 18:46:27 +0100

    Modified header of AoC 2022.

--- 2022/01/01.py
+++ 2022/01/01.py
@@ -1,20 +1,18 @@
-print("                                             ")
-print("         ***************************         ")
-print("         **  Advent of Code 2022  **         ")
-print("         ***************************         ")
-print("         **  Day 01: Sonar Sweep  **         ")
-print("         ***************************         ")
-print("                                             ")
+print()
+print(" ************************************ ")
+print(" **  AoC 2022_01: Inverse Captcha  ** ")
+print(" ************************************ ")
+print()
 
 filename = "01.input"
 with open(filename) as file:
     lines = file.read().splitlines()
 
 def part_one():
-    return true
+    return True
 
 def part_two():
-    return true
+    return True
 
 print("Part 1: ", part_one())
 print("Part 2: ", part_two())

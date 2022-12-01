e41076232d348e27806b0aefcbc44615bab86b9c
Manuel Schwarz <manschwa@uos.de>
2022-12-01 12:10:48 +0100

    day 02 preparations

---
+++ 2022/02/02.py
@@ -0,0 +1,18 @@
+print()
+print(" ************************************* ")
+print(" **  AoC 2022_02: ** ")
+print(" ************************************* ")
+print()
+
+filename = "02.input"
+with open(filename) as file:
+    lines = file.read().splitlines()
+
+def part_one():
+    return True
+
+def part_two():
+    return True
+
+print("Part 1: ", part_one())
+print("Part 2: ", part_two())

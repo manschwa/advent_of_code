f92a06f219f71b558533c03e350dc6b1e0aa474b
Manuel Schwarz <manschwa@uos.de>
2022-12-01 10:36:34 +0100

    adding solution for 2022_01

--- 2022/01/01.py
+++ 2022/01/01.py
@@ -1,18 +1,28 @@
 print()
-print(" ************************************ ")
-print(" **  AoC 2022_01: Inverse Captcha  ** ")
-print(" ************************************ ")
+print(" ************************************* ")
+print(" **  AoC 2022_01: Calorie Counting  ** ")
+print(" ************************************* ")
 print()
 
 filename = "01.input"
 with open(filename) as file:
     lines = file.read().splitlines()
 
+elves = [0]
+
 def part_one():
-    return True
+    elf = 0
+    for line in lines:
+        if line == '':
+            elf += 1
+            elves.append(0)
+        else:
+            elves[elf] += int(line)
+    return max(elves)
 
 def part_two():
-    return True
+    strongest_elves = sorted(elves)[-3:]
+    return sum(strongest_elves)
 
 print("Part 1: ", part_one())
 print("Part 2: ", part_two())

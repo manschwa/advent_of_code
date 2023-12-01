92d9f66d41ce88d4dcfc79c73bb425bccf5076bd
Manuel Schwarz <manschwa@uos.de>
2023-12-01 12:08:35 +0100

    adding day 2 structure + deleting day 1 input file

---
+++ 2023/02/02.py
@@ -0,0 +1,9 @@
+print()
+print(" *************************** ")
+print(" **  AoC 2023_02: Foobar  ** ")
+print(" *************************** ")
+print()
+
+filename = "02_sample.input"
+with open(filename) as file:
+    lines = file.read().splitlines()

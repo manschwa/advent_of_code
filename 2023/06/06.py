15a69692991ff3ce88d5fb93122e5ed1f796484e
Manuel Schwarz <manschwa@uos.de>
2023-12-07 11:45:02 +0100

    refactoring solution for 2023_06

--- 2023/06/06.py
+++ 2023/06/06.py
@@ -1,10 +1,12 @@
+import math
+
 print()
 print(" ******************************** ")
 print(" **  AoC 2023_06: Wait For It  ** ")
 print(" ******************************** ")
 print()
 
-filename = "06_sample.input"
+filename = "06.input"
 with open(filename) as file:
     lines = file.read().splitlines()
 
@@ -14,23 +16,21 @@ distances = list(map(int, lines[1].split(':')[1].split()))
 print(times)
 print(distances)
 
-def prod_ways_to_beat(times, distances):
-    prod = 1
-    for i in range(0, len(times)):
-        ways_to_beat = 0
-        for j in range(0, times[i]):
-            if j * (times[i] - j) > distances[i]:
-                ways_to_beat += 1
-        prod *= ways_to_beat
-    return prod
+def get_wins(time, distance):
+    x_1 = (time / 2) - math.sqrt((time / 2) ** 2 - distance)
+    return (time + 1) - (math.floor(x_1) + 1) * 2
 
 def part_one():
-    return prod_ways_to_beat(times, distances)
+    prod_wins = 1
+    for i in range(0, len(times)):
+        prod_wins *= get_wins(times[i], distances[i])
+    return prod_wins
 
 def part_two():
-    time = [int(''.join(map(str, times)))]
-    distance = [int(''.join(map(str, distances)))]
-    return prod_ways_to_beat(time, distance)
+    time = int(''.join(map(str, times)))
+    distance = int(''.join(map(str, distances)))
+    return get_wins(time, distance)
+
 
 print("Part 1: ", part_one())
 print("Part 2: ", part_two())

647257ef888f915b3f31293bfe3e5cfdba9821b9
Manuel Schwarz <manschwa@uos.de>
2023-12-07 11:46:34 +0100

    removing print statements in solution 2023_06

--- 2023/06/06.py
+++ 2023/06/06.py
@@ -13,9 +13,6 @@ with open(filename) as file:
 times = list(map(int, lines[0].split(':')[1].split()))
 distances = list(map(int, lines[1].split(':')[1].split()))
 
-print(times)
-print(distances)
-
 def get_wins(time, distance):
     x_1 = (time / 2) - math.sqrt((time / 2) ** 2 - distance)
     return (time + 1) - (math.floor(x_1) + 1) * 2
